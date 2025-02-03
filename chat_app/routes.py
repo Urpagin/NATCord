66b1596c424b8d878ca6a102318ffea9af05aeac
Urpagin <urpagin@proton.me>
2025-02-03 14:06:47 +0100

    simple polling system

--- chat_app/routes.py
+++ chat_app/routes.py
@@ -1,9 +1,30 @@
-from flask import render_template, Blueprint
+from flask import Flask, render_template, request, jsonify, Blueprint
 
-# Définition du Blueprint pour l'organisation du projet
 main = Blueprint('main', __name__)
+# Global list to store messages (for demonstration purposes only)
+messages = []
 
 @main.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@main.route('/channel')
+def channel():
+    return render_template('channel.html')
+
+@main.route('/send', methods=['POST'])
+def send_message():
+    msg = request.form.get('message')
+    if msg:
+        messages.append(msg)
+    # Return no content (HTTP 204) upon success
+    return ('', 204)
+
+@main.route('/poll')
+def poll():
+    # Return the full message list as JSON
+    return jsonify(messages)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
